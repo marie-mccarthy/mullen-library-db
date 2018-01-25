313c12299220bdc33d5477d3f3e39c7b700845f0
marie-mccarthy <marie.mccarthy2000@gmail.com>
2018-01-24 17:14:54 -0700

    changed to mySQL

--- test_library.sql
+++ test_library.sql
@@ -3,10 +3,10 @@ create schema if not exists test_library;
 -- List of users
 create table if not exists test_library.users (
   user_id serial primary key,
-  first_name varchar,
-  last_name varchar,
-  email varchar,
-  username varchar
+  first_name varchar(128),
+  last_name varchar(128),
+  email varchar(128),
+  username varchar(128)
 );
 
 -- insert into test_library.users (first_name, last_name, email, username) values
@@ -16,13 +16,13 @@ create table if not exists test_library.users (
 -- List of available books
 create table if not exists test_library.books (
   book_id serial primary key,
-  title varchar,
-  author varchar
+  title varchar(128),
+  author varchar(128)
 );
 
 -- insert into test_library.books (title, author) values
--- ('Harry Potter and the Philosophers Stone', 'J.K. Rowling')
--- ('The Great Gatsby', 'F. Scott Fitzgerald')
+-- ('Harry Potter and the Philosophers Stone', 'J.K. Rowling'),
+-- ('The Great Gatsby', 'F. Scott Fitzgerald');
 
 -- Join table for users to favorite books
 create table if not exists test_library.favorite_books (
@@ -31,6 +31,6 @@ create table if not exists test_library.favorite_books (
   book_id int
 );
 
--- insert into favorite_books (user_id, book_id) values
+-- insert into test_library.favorite_books (user_id, book_id) values
 -- (2, 1),
--- (2, 2)
+-- (2, 2);
